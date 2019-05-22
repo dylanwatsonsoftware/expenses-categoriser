bdb0bb30db47d2049e815bf35d5bbd693e1c9a0c
Dylan Watson <lotsabackscatter@gmail.com>
2019-05-22 19:52:14 +0800

    Add papa parse

--- src/app/expenses/Expenses/Expenses.vue
+++ src/app/expenses/Expenses/Expenses.vue
@@ -6,7 +6,7 @@
           <vue-headline level="1">Expenses</vue-headline>
         </vue-grid-item>
 
-        <vue-data-table :header="header" :data="data" placeholder="Search" @click="click" />
+        <vue-data-table :header="header" :data="data" :maxRows="100" placeholder="Search" @click="click"/>
       </vue-grid-row>
     </vue-grid>
   </div>
@@ -25,15 +25,17 @@ import VueHeadline from '@/app/shared/components/VueHeadline/VueHeadline.vue';
 import VueDataTable from '@/app/shared/components/VueDataTable/VueDataTable.vue';
 import { dataTableHeaderFixture, dataTableDataFixture } from '@/app/shared/components/VueDataTable/DataTableFixtures';
 
+import Papa from 'papaparse';
+
 export default {
-  metaInfo:   {
+  metaInfo: {
     title: 'Expenses',
   },
   data: () => {
     return {
       header: dataTableHeaderFixture,
-      data: dataTableDataFixture
-    }
+      data: dataTableDataFixture,
+    };
   },
   components: {
     VueGrid,
@@ -47,13 +49,35 @@ export default {
     ...mapActions('expenses', ['increment', 'decrement']),
     click(row: any) {
       alert(JSON.stringify(row));
-    }
+    },
+    async loadCSV() {
+      Papa.parse('/5524 1138 0113 3684_transaction_22_05_2019.csv', {
+        header: true,
+        download: true,
+        complete: (results) => {
+          console.log('results', results);
+          var header: any = {};
+          for (var key of Object.keys(results.data[0])) {
+            header[key] = {
+              title: key,
+            };
+          }
+          this.header = header;
+          this.data = results.data;
+
+          console.log(this.data);
+        },
+      });
+    },
   },
   computed: {
     ...mapGetters('expenses', ['count', 'incrementPending', 'decrementPending']),
   },
   beforeCreate() {
     registerModule('expenses', ExpensesModule);
+  },
+  created() {
+    this.loadCSV();
   },
   prefetch: (options: IPreLoad) => {
     registerModule('expenses', ExpensesModule);
@@ -63,7 +87,7 @@ export default {
 </script>
 
 <style lang="scss" module>
-@import "~@/app/shared/design-system";
+@import '~@/app/shared/design-system';
 
 .expenses {
   margin-top: $nav-bar-height;
