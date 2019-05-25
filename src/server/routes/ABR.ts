dc54b36cb08bd15e5f67431181580e53c03c6ce1
Dylan Watson <lotsabackscatter@gmail.com>
2019-05-25 23:15:21 +0800

    Stupidly simple cache

--- src/server/routes/ABR.ts
+++ src/server/routes/ABR.ts
@@ -4,8 +4,9 @@ import axios from 'axios';
 
 const abrCode = 'd775bcee-a0ba-4bb0-aee7-62b971918436';
 
-export const ABR = (app: Express.Application) => {
+const cache: any = {};
 
+export const ABR = (app: Express.Application) => {
   const fromJsonP = (jsonpData: string) => {
     var startPos = jsonpData.indexOf('({');
     var endPos = jsonpData.indexOf('})');
@@ -13,6 +14,11 @@ export const ABR = (app: Express.Application) => {
     return JSON.parse(jsonString);
   };
   app.get('/abr/names', (req: Request, res: Response) => {
+    if (cache[req.query.name]) {
+      res.status(200).json(cache[req.query.name]);
+      return;
+    }
+
     axios
       .get('https://abr.business.gov.au/json/MatchingNames.aspx', {
         params: {
@@ -32,6 +38,11 @@ export const ABR = (app: Express.Application) => {
   });
 
   app.get('/abr/abn', (req: Request, res: Response) => {
+    if (cache[req.query.abn]) {
+      res.status(200).json(cache[req.query.abn]);
+      return;
+    }
+
     axios
       .get('https://abr.business.gov.au/json/AbnDetails.aspx', {
         params: {
