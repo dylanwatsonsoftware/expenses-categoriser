d14123582da10ded2b597d37c4b5926495deb729
Dylan Watson <lotsabackscatter@gmail.com>
2019-05-25 21:51:05 +0800

    Add ABN number search

--- src/server/routes/ABR.ts
+++ src/server/routes/ABR.ts
@@ -5,6 +5,7 @@ import axios from 'axios';
 const abrCode = 'd775bcee-a0ba-4bb0-aee7-62b971918436';
 
 export const ABR = (app: Express.Application) => {
+
   const fromJsonP = (jsonpData: string) => {
     var startPos = jsonpData.indexOf('({');
     var endPos = jsonpData.indexOf('})');
@@ -18,6 +19,25 @@ export const ABR = (app: Express.Application) => {
           maxResults: req.query.maxResults,
           guid: abrCode,
           name: req.query.name,
+        },
+      })
+      .then((result) => {
+        let data = fromJsonP(result.data);
+        console.log(data);
+        res.status(200).json(data);
+      })
+      .catch((err) => {
+        res.status(400).json(JSON.parse(err));
+      });
+  });
+
+  app.get('/abr/abn', (req: Request, res: Response) => {
+    axios
+      .get('https://abr.business.gov.au/json/AbnDetails.aspx', {
+        params: {
+          maxResults: req.query.maxResults,
+          guid: abrCode,
+          abn: req.query.abn,
         },
       })
       .then((result) => {
